1ea3b7a6f19007050f2902bfd9ec363e41425b70
ALi fathi <alifathi8008@gmail.com>
2022-08-22 13:51:31 +0430

    FaceVerification: Move files into Code

--- Code/siamese.py
+++ Code/siamese.py
@@ -7,22 +7,22 @@ from matplotlib import pyplot as plt
 
 class Model:
     def __init__(self):
-        self.net =  self.build() 
+        self.net =  self.build()
         pass
-    
+
     def build(self):
         inp = ksl.Input([64,64,3])
         x = ksl.Conv2D(64,(3,3),padding='same',activation='relu')(inp)
         x = ksl.MaxPool2D(pool_size=[2,2])(x)
         x = ksl.Dropout(0.3)(x)
-        
+
         x = ksl.Conv2D(64,(2,2),padding='same',activation='relu')(x)
         x = ksl.MaxPool2D(pool_size=[2,2])(x)
         x = ksl.Dropout(0.3)(x)
         poolOut = ksl.GlobalAveragePooling2D()(x)
         x = ksl.Dense(256)(poolOut)
         model = tf.keras.Model(inp,x)
-        
+
         im1 = ksl.Input([64,64,3])
         im2 = ksl.Input([64,64,3])
         feature1 = model(im1)
@@ -31,11 +31,11 @@ class Model:
         net = tf.keras.Model([im1,im2],distance)
 
         return net
-    
+
     def compileModel(self):
         self.net.compile(loss='binart_crossentropy',optimizer='adam',metrics=['accuracy'])
-        
-    
+
+
     def train(self,images,target):
         self.net.fit(images,self.target,epochs=10,batch_size=32)
 
@@ -55,7 +55,7 @@ class Model:
     def euclidean_distance(vects):
         yA,yB = vects
         return tf.math.reduce_euclidean_norm(yA-yB,axis = 1,keepdims = True)
-        
+
     @staticmethod
     def controstivLoss(m = 1.0):
         def loss(yTrue,yPred):
