ff3a7a1b9b7d585f1d507a9586d69c824bda5b11
ALi fathi <alifathi8008@gmail.com>
2022-08-22 11:38:26 +0430

    FaceVerification: Improve utils

--- Code/utils.py
+++ Code/utils.py
@@ -8,9 +8,9 @@ class utils():
         imagesB = man['image_B']
         label = man['match']
         imagesA = imagesA.apply(lambda addr: self.loadAndPrerocess('../Data/'+addr))
-        imagesB.apply(lambda addr: self.loadAndPrerocess('../Data/'+addr))
+        imagesB = imagesB.apply(lambda addr: self.loadAndPrerocess('../Data/'+addr))
         return [imagesA,imagesB,label]
-        
+
     @staticmethod
     def loadAndPrerocess(path):
         image = cv.imread(path)
@@ -22,11 +22,10 @@ class utils():
         image = image/255.0
         return image
 
-    def Split(slef):
-        return train_test_split(pd.DataFrame,test_size=0.2)
+    @staticmethod
+    def Split(images,label):
+        return train_test_split(images,label,test_size=0.2)
+
 
     def visualizeData(self):
         pass
-
-u = utils()
-a = u.loadData('/home/alifathi/Documents/AI/Git/siameseNetwork/Data/verification_dev.csv')
